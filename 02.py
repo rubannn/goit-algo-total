cc4015ca950093c91c91bd326b1f5cf275af8fac
m.ruban <m.ruban@testportal.gov.ua>
2025-05-21 14:43:21 +0200

    task 02. added textinput for definition level

--- 02.py
+++ 02.py
@@ -5,24 +5,45 @@ import turtle
 import random
 
 
-def koch_curve(t, length, depth):
+def koch_curve(t, size, depth):
     if depth == 0:
-        t.forward(length)
+        t.forward(size)
     else:
-        length /= 3.0
-        koch_curve(t, length, depth - 1)
-        t.left(60)
-        koch_curve(t, length, depth - 1)
-        t.right(120)
-        koch_curve(t, length, depth - 1)
-        t.left(60)
-        koch_curve(t, length, depth - 1)
+        size /= 3.0
+        # koch_curve(t, size, depth - 1)
+        # t.left(60)
+        # koch_curve(t, size, depth - 1)
+        # t.right(120)
+        # koch_curve(t, size, depth - 1)
+        # t.left(60)
+        # koch_curve(t, size, depth - 1)
+
+        for angle in [60, -120, 60, 0]:
+            koch_curve(t, size, depth - 1)
+            t.left(angle)
+        # koch_curve(t, size, depth - 1)
 
 
-def draw_koch_snowflake(level):
+def draw_koch_snowflake():
     # Налаштування вікна
     window = turtle.Screen()
     window.bgcolor("white")
+
+    level_str = turtle.textinput("Рівень рекурсії", "Введіть рівень рекурсії (1-5):")
+
+    if level_str is None:  # Якщо натиснуто Cancel
+        turtle.bye()
+        return None
+
+    try:
+        level = int(level_str)
+        if level < 1 or level > 5:
+            raise ValueError("Рівень рекурсії повинен бути від 1 до 5.")
+    except ValueError as e:
+        turtle.bye()
+        print(f"Помилка: {e}")
+        return None
+
     window.title(f"Сніжинка Коха ({level=})")
 
     # Створення черепашки
@@ -48,5 +69,6 @@ def draw_koch_snowflake(level):
 
 # Запуск програми
 if __name__ == "__main__":
-    level = random.randint(1, 5)  # Випадковий рівень рекурсії від 1 до 5
-    draw_koch_snowflake(level)
+    # level = random.randint(1, 5)  # Випадковий рівень рекурсії від 1 до 5
+    # level = int(input("Введіть рівень рекурсії (1-5): "))
+    draw_koch_snowflake()
